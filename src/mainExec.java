97408ef97f7f5cb01b846aeb77a1f384c6edfc47
Christopher Everett <cce29555@gmail.com>
2019-08-28 22:36:14 -0400

    bishop bug
    Fixed a bug where bishops would take the place of knights, kings, and queens
    
    Needless to say this is not intended and I have put the bishops in their place. Everything generates correctly except maybe the opposing teams King

--- src/mainExec.java
+++ src/mainExec.java
@@ -8,7 +8,7 @@ public class mainExec {
 	//Spawns all the pieces, bound is set to 32 so each piece gets its own ID, this will become a mess later
 	//Thankfully everything is grouped by type so this is where IDs come in
 	//by checking IDs I can keep track of types so I can push them to either side of the board slightly easier
-	static Pieces []pawn = new Pieces[32]; 		//Spawns an empty object for Pawns
+	static Pieces []pawn = new Pieces[32]; 			//Spawns an empty object for Pawns
 	static Pieces []rook = new Pieces[32]; 			//Spawns an empty object for Rooks
 	static Pieces []bishop = new Pieces[32]; 		//Spawns an empty object for Bishops
 	static Pieces []knight = new Pieces[32]; 		//Spawns an empty object for Knights
@@ -26,10 +26,15 @@ public class mainExec {
 		Pieces []rook = new Pieces[4]; 			//Spawns an empty object for Rooks
 		Pieces []bishop = new Pieces[4]; 		//Spawns an empty object for Bishops
 		Pieces []knight = new Pieces[4]; 		//Spawns an empty object for Knights
-		Pieces []king = new Pieces[2]; 			//Spawns an empty object for Kings
+		Pieces []king = new Pieces[2]; 			//Spawns an empty object fo	r Kings
 		Pieces []queen = new Pieces[2];			//Spawns an empty object for Queens*/
 		//------------------------------------------
 		//16+4+4+4+2+2 = 32, thanks for coming to my ted talk
+		
+		//For some reason bishops have dominated all bounds above their limits, so bug fixing is on that right now 8/28/2019 10:25
+		//Bishop bug fixed 8/28/2019 10:33
+		//Meaning I could've just set IDs and kept them as "Pieces" instead of identifying each one, but it's more for me than the compiler
+		//Nothing will be changed as IDs and Colors were assigned correctly
 			for (x = 0; x < 32; x++)
 				{
 					if (x <= 16) 						//Generate Pawns as long as they're under 16
@@ -55,7 +60,7 @@ public class mainExec {
 								rook[x].setColor(1); 	//White Rooks!!!
 							}
 						}
-					if (x >= 21 || x <= 24)
+					if (x <= 21 || x <= 24)
 						{
 							bishop[x] = new Pieces();  	//Generate Bishops
 							bishop[x].setIndex(x);		//Rook Index for positioning
@@ -66,37 +71,37 @@ public class mainExec {
 								bishop[x].setColor(1);	//Make em White
 							}
 						}
-					if (x >= 25 || x <= 28)
+					if (x <= 25 || x <= 28)
 						{
-							knight[x] = new Pieces();
-							knight[x].setIndex(x);
-							knight[x].setID(4);
-							knight[x].setColor(0);
+							knight[x] = new Pieces();	//generate knights
+							knight[x].setIndex(x);		//knights indices
+							knight[x].setID(4);			//knights IDs	
+							knight[x].setColor(0);		//Knights Whites
 							if (x >= 27)
 							{
-								knight[x].setColor(1);
+								knight[x].setColor(1);	//Knacks Blacks
 							}
 						}
-					if (x >= 29 || x <= 30)
+					if (x <= 29 || x <= 30)
 						{
-							queen[x] = new Pieces();
-							queen[x].setIndex(x);
-							queen[x].setID(5);
-							queen[x].setColor(0);
+							queen[x] = new Pieces();	//Generate some queens
+							queen[x].setIndex(x);		//Give em an index in the 29/30 range
+							queen[x].setID(5);			//Queen IDs
+							queen[x].setColor(0);		//Queens are Black!!
 							if (x == 30)
 							{
-								queen[x].setColor(1);
+								queen[x].setColor(1);	//There are white queens too
 							}
 						}
-					if (x >=31)
+					if (x <=31)
 						{
-							king[x] = new Pieces();
-							king[x].setIndex(x);
-							king[x].setID(6);
-							king[x].setColor(0);
+							king[x] = new Pieces();		//Generate some Kings
+							king[x].setIndex(x);		//Give em an index in the 31/32 range
+							king[x].setID(6);			//Kings have an index of 6
+							king[x].setColor(0);		//Kings are black
 							if (x == 32)
 							{
-								king[x].setColor(1);
+								king[x].setColor(1);	//Kings are white
 							}
 						}
 				}
@@ -123,7 +128,9 @@ public class mainExec {
 		//This entire function is for me to plug in at random moments, the debugger in eclipse isn't working how I'm used to
 		//So I'll just activate this and waste an extra 30 minutes making sure all the variables are working correctly
 		//Thanks eclipse
-		for (x = 0; x < 32; x++)
+		
+		//System.out.println("Queen ID is " + pawn[29].getId() + "and Index is: " + queen[2].getIndex());
+		for (x = 0; x <= 36; x++)
 		{
 			System.out.println("X is at: " + x);
 			while (x <= 16) 						//Generate Pawns as long as they're under 16
@@ -136,32 +143,34 @@ public class mainExec {
 				System.out.println("Rook Stats, ID: " + rook[x].getId() + " index: " + rook[x].getIndex() + " color:" + rook[x].getColor());
 				++x;
 			}
-			while (x >= 21 || x <= 24)
+			while (x <= 21 || x <= 24)
 			{
 				System.out.println("Bishop Stats, ID: " + bishop[x].getId() + " index: " + bishop[x].getIndex() + " color:" + bishop[x].getColor());
 				++x;
 			}
-			while (x >= 25 || x <= 28)
+			while (x <= 25 || x <= 28)
 			{
 				System.out.println("Knight Stats, ID: " + knight[x].getId() + " index: " + knight[x].getIndex() + " color:" + knight[x].getColor());
 				++x;
 			}
-			while (x >= 29 || x <= 30)
+			while (x <= 29 || x <= 30)
 			{
 				System.out.println("queen Stats, ID: " + queen[x].getId() + " index: " + queen[x].getIndex() + " color:" + queen[x].getColor());
 				++x;
 			}
-			while (x >=31)
+			while (x <= 31)
 			{
 				System.out.println("king Stats, ID: " + king[x].getId() + " index: " + king[x].getIndex() + " color:" + king[x].getColor());
 				++x;
+				
 			}
+			
 		}
 	}
 	public static void main(String[] args) {
 		createBoard();
 		createPieces();
-		//debugOutput();
+		debugOutput();
 			
 	}
 	
